bb20772f69fc6b5817cadc55a440bf283ea359f7
Plamen Duchev <plamen.duchev@sumup.com>
2017-09-07 15:06:45 +0300

    Fix the id-s

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,26 +1,24 @@
 import "phoenix_html"
 import {Socket, Presence} from "phoenix"
 
-// Socket
-let user = document.getElementById("User").innerText
+let user = document.getElementById("user").innerText
 let socket = new Socket("/socket", {params: {user: user}})
 socket.connect()
 
-// Presence
 let presences = {}
 
-let formatTimestamp = (timestamp) => {
-  let date = new Date(timestamp)
+let formatedTimestamp = (Ts) => {
+  let date = new Date(Ts)
   return date.toLocaleTimeString()
 }
 let listBy = (user, {metas: metas}) => {
   return {
     user: user,
-    onlineAt: formatTimestamp(metas[0].online_at)
+    onlineAt: formatedTimestamp(metas[0].online_at)
   }
 }
 
-let userList = document.getElementById("UserList")
+let userList = document.getElementById("userList")
 let render = (presences) => {
   userList.innerHTML = Presence.list(presences, listBy)
     .map(presence => `
@@ -32,8 +30,7 @@ let render = (presences) => {
     .join("")
 }
 
-// Channels
-let room = socket.channel("room:lobby", {})
+let room = socket.channel("room:lobby")
 room.on("presence_state", state => {
   presences = Presence.syncState(presences, state)
   render(presences)
@@ -46,8 +43,7 @@ room.on("presence_diff", diff => {
 
 room.join()
 
-// Chat
-let messageInput = document.getElementById("NewMessage")
+let messageInput = document.getElementById("newMessage")
 messageInput.addEventListener("keypress", (e) => {
   if (e.keyCode == 13 && messageInput.value != "") {
     room.push("message:new", messageInput.value)
@@ -55,12 +51,12 @@ messageInput.addEventListener("keypress", (e) => {
   }
 })
 
-let messageList = document.getElementById("MessageList")
+let messageList = document.getElementById("messageList")
 let renderMessage = (message) => {
   let messageElement = document.createElement("li")
   messageElement.innerHTML = `
     <b>${message.user}</b>
-    <i>${formatTimestamp(message.timestamp)}</i>
+    <i>${formatedTimestamp(message.timestamp)}</i>
     <p>${message.body}</p>
   `
   messageList.appendChild(messageElement)
